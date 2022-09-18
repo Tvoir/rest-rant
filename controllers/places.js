d8fa6041996c483b7c9d272419f7964aa6c1ee7b
Tvoir <TommyVoir@yahoo.com>
2022-09-17 23:48:49 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 //More code in a moment
 
@@ -7,25 +8,37 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  // console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 
+
 router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/restaurant.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/bar.jpg'
-  }]
+  // let places = [{
+  //   name: 'H-Thai-ML',
+  //   city: 'Seattle',
+  //   state: 'WA',
+  //   cuisines: 'Thai, Pan-Asian',
+  //   pic: '/images/restaurant.jpg'
+  // }, {
+  //   name: 'Coding Cat Cafe',
+  //   city: 'Phoenix',
+  //   state: 'AZ',
+  //   cuisines: 'Coffee, Bakery',
+  //   pic: '/images/bar.jpg'
+  // }]
   
   res.render('places/index', { places })
   

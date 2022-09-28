715f9d73ecc581331476c9e189e8c2aafea6ed7b
Tvoir <TommyVoir@yahoo.com>
2022-09-27 23:16:36 -0700

    trying to fix path for comment section

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,6 +3,25 @@ const Def = require('../default')
 
     
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            no comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+          )
+        })
+      }
     return (
         <Def>
           <main>
@@ -31,9 +50,34 @@ function show (data) {
                 </form> 
                 </div>
             </div>
+            <hr />
+            <h2>Comments</h2>
+            {comments}
+            <hr />
+            <h2> Got Something to say?</h2>
+            <form method="POST" action="/places">
+                    <div className="form-group">
+                        <label htmlFor="content">Content</label>
+                        <input className="form-control" id="content" name="content" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="author">Author</label>
+                        <input className="form-control" id="author" name="author" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="rating">Rating</label>
+                        <input className="form-control" id="rating" name="rating" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="rant?">Rant?</label>
+                        <input className="form-control" type= "checkbox" id="state" name="state" />
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Add Comment" />
+                </form>
             </main>
         </Def>
     )
 }
 
 module.exports = show
+

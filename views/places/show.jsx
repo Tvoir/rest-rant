ddca25227c0a5e35e6618de834e7d7f95ad8576a
Tvoir <TommyVoir@yahoo.com>
2022-09-29 21:35:52 -0700

    finished hw

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,7 +8,25 @@ function show (data) {
             no comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length) 
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+        }
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
           return (
             <div className="border">
@@ -33,21 +51,27 @@ function show (data) {
                     </h3>
                 </div>
                 <div className="col-sm-6">
-                <h2>Description</h2>
-                <h3>
-                    {data.place.showEstablished()}
-                </h3>
-                <h4>
-                    Serving {data.place.cuisines}
-                </h4>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-                </a>     
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">
-                Delete
-                </button>
-                </form> 
+                    <h1>{ data.place.name }</h1>
+                    <h2>
+                        Rating
+                    </h2>
+                    {rating}
+                    <br />
+                    <h2>Description</h2>
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                        Serving {data.place.cuisines}
+                    </h4>
+                    <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
+                        Edit
+                    </a>     
+                    <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
+                        <button type="submit" className="btn btn-danger">
+                            Delete
+                        </button>
+                    </form> 
                 </div>
             </div>
             <hr />
@@ -82,4 +106,3 @@ function show (data) {
 }
 
 module.exports = show
-

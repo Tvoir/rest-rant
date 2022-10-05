eac72fbf443b22d57d2c94179e9f5bf8ed7a573d
Tvoir <TommyVoir@yahoo.com>
2022-10-04 22:22:38 -0700

    heroku

--- controllers/places.js
+++ controllers/places.js
@@ -1,31 +1,27 @@
 const router = require('express').Router()
 const db = require('../models')
-// const places = require('../models/places.js')
 
-//  More code in a moment 
 router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-      res.render('places/index', { places })
-    })
-    .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
+  db.Place.find()
+  .then((places) => {
+    res.render('places/index', { places })
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
 })
-
 
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
-      res.redirect('/places')
+    res.redirect('/places')
   })
   .catch(err => {
-      console.log('err', err)
-      res.render('error404')
+    console.log('err', err)
+    res.render('error404')
   })
 })
-
 
 router.get('/new', (req, res) => {
   res.render('places/new')
@@ -35,7 +31,6 @@ router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
   .populate('comments')
   .then(place => {
-    console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -43,6 +38,7 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
+
 
 router.put('/:id', (req, res) => {
   db.Place.findByIdAndUpdate(req.params.id, req.body)
@@ -68,7 +64,6 @@ router.delete('/:id', (req, res) => {
 })
 
 
-
 router.get('/:id/edit', (req, res) => {
   db.Place.findById(req.params.id)
   .then(place => {
@@ -80,38 +75,50 @@ router.get('/:id/edit', (req, res) => {
 })
 
 
-router.post('/:id/comment', (req, res) => {
-  if (req.body.author === '') { req.body.author = undefined }
-  req.body.rant = req.body.rant ? true : false
-  console.log(req.body)
-  db.Place.findById(req.params.id)
-    .then(place => {
-      db.Comment.create(req.body)
-        .then(comment => {
-          place.comments.push(comment.id)
-          place.save()
-            .then(() => {
-              res.redirect(`/places/${req.params.id}`)
-            })
-          })
-          .catch(err => {
-            res.render('error404')
-          })
-    })
-      .catch(err => {
-    // console.log("Working!")
-      res.render('error404')
-    })
-})
-
-
 // router.post('/:id/rant', (req, res) => {
-//   console.log(req.body)
 //   res.send('GET /places/:id/rant stub')
 // })
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+// router.delete('/:id/rant/:rantId', (req, res) => {
+//     res.send('GET /places/:id/rant/:rantId stub')
+// })
+
+router.post('/:id/comment', (req, res) => {
+  console.log('post comment', req.body)
+  if (req.body.author === '') { req.body.author = undefined }
+  req.body.rant = req.body.rant ? true : false
+  db.Place.findById(req.params.id)
+      .then(place => {
+          db.Comment.create(req.body)
+              .then(comment => {
+                  place.comments.push(comment.id)
+                  place.save()
+                      .then(() => {
+                          res.redirect(`/places/${req.params.id}`)
+                      })
+                      .catch(err => {
+                          res.render('error404')
+                      })
+              })
+              .catch(err => {
+                  res.render('error404')
+              })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+})
+
+router.delete('/:id/comment/:commentId', (req, res) => {
+  db.Comment.findByIdAndDelete(req.params.commentId)
+      .then(() => {
+          console.log('Success')
+          res.redirect(`/places/${req.params.id}`)
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
 })
 
 module.exports = router
